ed594683f5d8fddddbd56f1a0d808a569a92b70a
JK <kuku6471@gmail.com>
2021-08-25 16:38:57 +0900

    componnents refactoring final

--- .history/src/components/base_20210825163604.ts
+++ .history/src/components/base_20210825163604.ts
@@ -1,11 +1,16 @@
+
 export class BaseComponent<T extends HTMLElement> {
+>>>>>>> 8e96661 (componnents refactoring)
   protected readonly element: T;
   constructor(htmlstring: string) {
     const template = document.createElement("template");
     template.innerHTML = htmlstring;
     this.element = template.content.firstElementChild! as T;
   }
+<<<<<<< HEAD
+=======
 
+>>>>>>> 8e96661 (componnents refactoring)
   attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
     parent.insertAdjacentElement(position, this.element);
   }
--- .history/src/components/page/page_20210825163552.ts
+++ .history/src/components/page/page_20210825163552.ts
@@ -2,5 +2,6 @@ import { BaseComponent } from "../base.js";
 export class PageComponets extends BaseComponent<HTMLUListElement> {
   constructor() {
     super(`<ul class="page">Hello world</ul>`);
+>>>>>>> 8e96661 (componnents refactoring)
   }
 }
